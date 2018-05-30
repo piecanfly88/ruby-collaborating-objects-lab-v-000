af5463586dc40b55626704edcb96a1ef551aa6c5
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-05-30 08:17:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,6 +8,6 @@ class Song
   def self.new_by_filename(filename)
     song_artist, song = filename.split(" - ")
     new_song = self.new(song)
-    Artist.song_artist = new_song
+    Artist.name = song_artist
   end
 end
