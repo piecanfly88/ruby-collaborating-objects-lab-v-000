ed7bbb625b88cd5ded27cc6f1e3c8369879848b7
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-05-30 08:00:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,11 @@
 class Song
   attr_accessor :name, :artist
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name = name
+  end
 
-def self.new_by_filename(filename)
-
-  song_name = filename.split(" - ")[1]
-  song = self.new(song_name)
-end
-
-
+  def self.new_by_filename(filename)
+    song = self.new(filename)
+  end
 end
